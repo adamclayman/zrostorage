f400ebb65853b784969ce065bda50630ef33a9d4
Adam Clayman <adam@clayman.org>
2018-01-02 10:22:20 -0600

    3.9 Added 'message_for_men', Edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'simple_pages/about'
 
-  get 'simple_pages/contact'
+  root 'simple_pages#index'
 
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+  get 'simple_pages/message_for_men'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

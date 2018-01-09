537e6132adbbecedfe3df974d26ec81e0b438af9
Adam Clayman <adam@clayman.org>
2018-01-09 16:39:05 -0600

    Built orders controller, associated views (index, show, create, destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
-  resources :products
+
   root 'simple_pages#index'
 
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/login'
+
+  resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

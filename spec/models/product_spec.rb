ce15c6596194715540f9adef09c50a22adcb84e9
Adam Clayman <adam@clayman.org>
2018-02-05 08:46:26 -0600

    6.2: Started product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,4 +9,13 @@ describe Product do
 		product.comments.create!(rating: 3, user: user, body: "Ok bike!")
 		product.comments.create!(rating: 5, user: user, body: "Great bike!")
 	end
+
+	if "returns the average rating of all comments" do
+		
+	end
+
+	if "shows that a product without a name is invalid" do
+		expect(Product.new(description: "Nice bike")).not_to be_valid
+	end
+
 end

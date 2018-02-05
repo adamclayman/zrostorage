3a217a44c11073515219b5781cfe2e88be6273df
Adam Clayman <adam@clayman.org>
2018-02-05 11:38:08 -0600

    6.2: Modified product_spec.rb in response to error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,19 +3,17 @@ require 'rails_helper'
 describe Product do
 	let(:product) { Product.create!(name: "race bike")}
 	let(:user) {User.create!(email: "adam+7@clayman.org", password: "Password")}
-
 	before do
 		product.comments.create!(rating: 1, user: user, body: "Awful bike!")
 		product.comments.create!(rating: 3, user: user, body: "Ok bike!")
 		product.comments.create!(rating: 5, user: user, body: "Great bike!")
 	end
 
-	if "returns the average rating of all comments" do
+	it "returns the average rating of all comments" do
 		
 	end
 
-	if "shows that a product without a name is invalid" do
+	it "shows that a product without a name is invalid" do
 		expect(Product.new(description: "Nice bike")).not_to be_valid
 	end
-
 end

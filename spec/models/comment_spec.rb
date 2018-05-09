9969254ed683dab2b27e732e3a8e36de75f6cddd
Clayman <clayman@claymans-air.attlocal.net>
2018-05-09 19:03:55 -0400

    Deleted unnecessary before-do requirement from Comment Factory Spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,11 +3,6 @@ require 'rails_helper'
 describe Comment, type: :model do
 	let(:product) { Product.create!(name: "race bike", description: "a race bike description", price: 55.00)}
 	let(:user) {User.create!(email: "adam+7@clayman.org", password: "Password")}
-	before do
-		product.comments.create!(rating: 1, user: user, body: "Awful bike!")
-		product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-		product.comments.create!(rating: 5, user: user, body: "Great bike!")
-	end
 
 	it "should validate a comment with full entry data" do
 		comment = FactoryBot.build(:comment)

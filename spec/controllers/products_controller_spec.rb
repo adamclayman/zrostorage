ab4213391f6c310d7dec92160f9de496c734593f
Adam <adam@clayman.org>
2018-05-10 08:43:30 -0400

    Added detail to product controller test spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -26,27 +26,84 @@ describe ProductsController, type: :controller do
 	end
 
 
-	describe 'POST #new' do
-		before do
-			sign_in @user
-		end
-
-		it 'posts correct new product' do
-			post :new, params: { name: "10 x 20 Locker", description: "10x20 storage unit.", image_url: "/images/10x20.png", price: "130.00" }
-			expect(response).to be_ok
-		end
-	end
 
 	describe 'GET #edit' do
+		context 'when an admin edits a product' do
+			before do
+				sign_in admin
+				it 'renders the edit template' do
+					get :edit, params: { id: @product.id }
+					expect(assigns(:product)).to eq @product
+					expect(response).to redirect_to('edit')
+				end
+			end
+		end
+
+		context 'when a non-admin user edits a product' do
+			before do
+				sign_in user
+				it 'redirects her/him to root path' do
+					get :edit, params: { id: @product.id }
+					expect(assigns(:product)).to eq @product
+					expect(response).to redirect_to(root_path)
+				end
+			end
+		end
+
+		context 'when anyone not signed in edits a product' do
+			it 'redirects her/him to root path' do
+				get :edit, params: { id: @product.id }
+				expect(assigns(:product)).to eq @product
+				expect(response).to redirect_to(+ '/login')
+			end
+		end
 	end
 
 	describe 'POST #create' do
+		context 'when an admin submits a new product' do		
+			before do
+				sign_in admin
+				it 'creates a new product if valid' do
+					post :create, params: { product: attributes_for(product) }
+					expect(status).to be_ok
+				end
+
+				it 'rejects a new product if invalid' do
+					post :create, params: { product: attributes_for(product) }
+					expect(status).not_to be_ok
+				end
+			end
+		end
 	end
 
-	describe 'PATCH #update' do
+
+	describe 'PUT #update' do
+		context 'when an admin submits an update to a product' do
+			before do
+				sign_in admin
+				it 'updates product if valid' do
+					put :update, params: { product: product }
+					expect(status).to be_ok
+					expect(response).to redirect_to('show')
+				end
+
+				it 'does not update product if invalid' do
+					put :update, params: { product: invalid_product }
+					expect(response).not_to be_ok
+					expect(response).to redirect_to('edit')
+				end
+			end
+		end
 	end
 
-	describe 'DELETE #destroy' do
-	end
+	context 'DELETE #destroy' do
+		before do
+			let(:product_to_delete) { FactoryBot.create(:product) }
 
+			it 'deletes product by id' do
+				delete :destroy, params: { product: product_to_delete }
+				expect(notice).to eq 'Product was successfully destroyed.'
+			end
+		end
+	end
 end
